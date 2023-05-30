2dd710deb6dc5430c31b248259b67de0f04361e4
Angel Eduardo <40308240+Anedu91@users.noreply.github.com>
2023-05-30 09:10:33 -0300

    polish screens

--- Dictionary-client/client/src/components/Card/SearchCard.tsx
+++ Dictionary-client/client/src/components/Card/SearchCard.tsx
@@ -1,106 +1,155 @@
-import {Button,Container,List,ListItem,ListItemText,Grid,Box} from '@mui/material';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import {SearchResult} from '../Layout/Layout' 
-import { useState } from 'react';
-import VolumeUpIcon from '@mui/icons-material/VolumeUp';
+import {
+  Button,
+  Container,
+  List,
+  ListItem,
+  ListItemText,
+  Grid,
+  Box,
+} from "@mui/material";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import { useState } from "react";
+import VolumeUpIcon from "@mui/icons-material/VolumeUp";
+import { WordInfo } from "../Layout/Layout";
 
 type Definition = {
-  word: string,
-  definition: string[]
-  audio:string[]
-  example:string[]
-  pronunciation:string[]
-  
-}
+  word: string;
+  definition: string[];
+  audio: string[];
+  example: string[];
+  pronunciation: string[];
+};
 
 type SearchCardProps = {
-  searchWord:string
-  wordInfo: Definition
-  handleResetSearch: () => void 
-  word:string
-  onSearch: (data:SearchResult) => void
-}
+  wordInfo: WordInfo;
+  handleResetSearch: () => void;
+  setNewWord: any;
+};
 
-export default function SearchCard({searchWord,wordInfo,handleResetSearch}:SearchCardProps) {
-  const [isPlayingAudio,setIsPlayingAudio] = useState(false)
-  console.log(isPlayingAudio)
+export default function SearchCard({
+  wordInfo,
+  handleResetSearch,
+  setNewWord,
+}: SearchCardProps) {
+  const [, setIsPlayingAudio] = useState(false);
 
   const playAudio = () => {
-    if(wordInfo?.audio){
-      const audio = new Audio(wordInfo.audio[0])
-      audio.play()
-      setIsPlayingAudio(true)
-      setTimeout(()=>{
-        audio.pause()
-        setIsPlayingAudio(false)
-      },3000)
+    if (wordInfo?.audio) {
+      const audio = new Audio(wordInfo.audio[0]);
+      audio.play();
+      setIsPlayingAudio(true);
+      setTimeout(() => {
+        audio.pause();
+        setIsPlayingAudio(false);
+      }, 3000);
     }
-
-  }
-
+  };
 
   return (
-    <Grid container justifyContent='center' alignItems='center' spacing={2}>
-  <Grid item xs={12}>  
-    <CardContent>    
-      {searchWord && (              
-        <Container sx={{borderBottom:'0.05rem solid'}}>
-          <Typography variant="h5" sx={{marginBottom:'0.25rem',fontWeight:'bold'}}>
-            {searchWord.toUpperCase()}
-          </Typography>
-          <Grid container justifyContent='space-between' alignItems='center'>
-            <Grid item>
-              <Typography sx={{ color:'secondary.main' }}>
-                {wordInfo.pronunciation[0]}                    
-              </Typography>
+    <Grid container justifyContent="center" alignItems="center" spacing={2}>
+      <Grid item xs={12}>
+        <CardContent>
+          <Container sx={{ borderBottom: "0.05rem solid" }}>
+            <Typography
+              variant="h5"
+              sx={{ marginBottom: "0.25rem", fontWeight: "bold" }}
+            >
+              {wordInfo.word.toUpperCase()}
+            </Typography>
+            <Grid container justifyContent="space-between" alignItems="center">
+              <Grid item>
+                <Typography sx={{ color: "secondary.main" }}>
+                  {wordInfo.pronunciation[0]}
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Button>
+                  <VolumeUpIcon onClick={playAudio} />
+                </Button>
+              </Grid>
             </Grid>
-            <Grid item>
-              <Button>
-                <VolumeUpIcon onClick={playAudio} />
-              </Button>
-            </Grid>                  
-          </Grid> 
-        </Container>
-      )}  
-      <Container>
-        {searchWord && (
-          <>
-            <Typography sx={{fontWeight:'bold'}}>Meaning</Typography>
-            <List>
-              {wordInfo.definition.map((el,index)=>(
-                <ListItem key={index} sx={{ "&::before": { content: "'\\2022'", marginRight: "8px", fontSize: "2rem", color: "secondary.main" } }}>
-                  <ListItemText primary={el}/>
-                </ListItem>
-              ))}
-            </List>  
-          </>
-        )}
-        {searchWord && (
-          <>
-            <Typography sx={{fontWeight:'bold'}}>Examples</Typography>  
-            <List>
-              {wordInfo.example.map((el,index)=>(
-                <ListItem key={index} sx={{ "&::before": { content: "'\\2022'", marginRight: "8px", fontSize: "2rem", color: "secondary.main" } }}>
-                  <ListItemText primary={el}/>
-                </ListItem>
-              ))}
-            </List>
-          </>
-        )}                 
-      </Container>
-    </CardContent> 
-  </Grid>
-  {searchWord && (
-    <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: '1rem' }}>
-      <Button onClick={handleResetSearch} variant='contained' color='secondary'>
-        <Typography sx={{fontWeight:'bold'}}>
-          Find a new Word!
-        </Typography>
-      </Button>      
-    </Box> 
-  )}
-</Grid>
-  )}
+          </Container>
 
+          <Container>
+            <>
+              <Typography sx={{ fontWeight: "bold" }}>Meaning</Typography>
+              <List>
+                {wordInfo.definition.map((el, index) => {
+                  console.log(el.split(" "));
+                  return (
+                    <ListItem
+                      key={index}
+                      sx={{
+                        display: "flex",
+                        gap: "5px",
+                        "&::before": {
+                          content: "'\\2022'",
+                          marginRight: "8px",
+                          fontSize: "2rem",
+                          color: "secondary.main",
+                        },
+                      }}
+                    >
+                      {el.split(" ").map((word, index) => {
+                        if (word.length <= 2) {
+                          return <p key={index}>{`${word}`}</p>;
+                        } else {
+                          return (
+                            <Typography
+                              sx={{
+                                cursor: "pointer",
+                              }}
+                              onClick={(event) => {
+                                setNewWord(event.target.innerHTML);
+                              }}
+                              key={index}
+                            >{`${word}`}</Typography>
+                          );
+                        }
+                      })}
+                      {/* <ListItemText primary={el} /> */}
+                    </ListItem>
+                  );
+                })}
+              </List>
+            </>
 
+            <>
+              <Typography sx={{ fontWeight: "bold" }}>Examples</Typography>
+              <List>
+                {wordInfo.example.map((el, index) => (
+                  <ListItem
+                    key={index}
+                    sx={{
+                      "&::before": {
+                        content: "'\\2022'",
+                        marginRight: "8px",
+                        fontSize: "2rem",
+                        color: "secondary.main",
+                      },
+                    }}
+                  >
+                    <ListItemText primary={el} />
+                  </ListItem>
+                ))}
+              </List>
+            </>
+          </Container>
+        </CardContent>
+      </Grid>
+
+      <Box
+        sx={{ display: "flex", justifyContent: "center", marginTop: "1rem" }}
+      >
+        <Button
+          onClick={handleResetSearch}
+          variant="contained"
+          color="secondary"
+        >
+          <Typography sx={{ fontWeight: "bold" }}>Find a new Word!</Typography>
+        </Button>
+      </Box>
+    </Grid>
+  );
+}
